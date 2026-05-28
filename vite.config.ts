a72131bf07c4f8a8613a6cbac2d38c56c1690288
edtheeagle26 <edtheeagle26@gmail.com>
2026-05-28 09:44:47 +0200

    Add @assets alias to Vite config

--- vite.config.ts
+++ vite.config.ts
@@ -1,12 +1,14 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
+import path from "path";
 
 export default defineConfig({
   plugins: [react()],
   resolve: {
     alias: {
-      "@": "/src"
+      "@": "/src",
+      "@assets": path.resolve(__dirname, "src/assets")
     }
-  },
-  base: "/"
+  }
 });
+
